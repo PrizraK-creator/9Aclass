d5364e640373cae34349b50b5dd9756f9fc69de7
PrizraK-creator <78219653+PrizraK-creator@users.noreply.github.com>
2021-02-16 17:10:12 +0200

    ff

--- script.js
+++ script.js
@@ -25,23 +25,3 @@ function check() {
 	}
 }
 
-
-var name = $('input[name=fio]').val();
- var tel = $('input[name=tel]').val();
- var otpravka = true;
- if(name==""){
- otpravka = false;
- }
- if(tel ==""){
- otpravka = false;
- }
-
-dannie = {'polz_name':name, 'polz_tel':tel};
-
-if(otpravka)
- {
- $.post('send.php', dannie, function(otvet){
- rezultat = '<div>'+otvet.text+'</div>';
- $("#form_result").hide().html(rezultat).slideDown();
- }, 'json');
- }
